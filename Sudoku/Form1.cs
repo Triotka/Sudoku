631232bbdf59fc3b1e54fd1e43260a6752367a8c
Triotka <prokopova.kristyna@email.cz>
2021-06-18 16:49:13 +0200

    written createSudoku() and reacting to typing

--- Sudoku/Form1.cs
+++ Sudoku/Form1.cs
@@ -10,12 +10,83 @@ using System.Windows.Forms;
 
 namespace Sudoku
 {
+    public class Grid : Button
+    {
+        public int numberInRow;
+        public int numberInCol;
+
+       
+    }
     public partial class Form1 : Form
     {
+        const int sudokuSize = 9;
+        const int sizeOfGrid = 40;
+        public int[,] numbers = new int[sudokuSize, sudokuSize];
+        public Grid[,] grids = new Grid[sudokuSize, sudokuSize];
+
+        public void updateGridText()
+        {
+            for (int r = 0; r < sudokuSize; r++)
+            {
+                for (int c = 0; c < sudokuSize; c++)
+                {
+                    if (numbers[r, c] == 0)
+                    {
+                        grids[r, c].Text = "";
+                    }
+                    else
+                    {
+                        grids[r,c].Text = numbers[r, c].ToString();
+                    }
+                }
+            }
+            
+        }
+        public void CreateSudoku()
+        {
+            for (int r = 0; r < sudokuSize; r++)
+            {
+                for(int c = 0; c < sudokuSize; c++)
+                {
+                    grids[r,c] = new Grid();
+                    grids[r, c].Location = new Point(r * sizeOfGrid, c * sizeOfGrid);
+                    grids[r, c].Size = new Size(sizeOfGrid, sizeOfGrid);
+                    grids[r, c].KeyPress += cell_keyPressed;
+                    grids[r, c].numberInRow = r;
+                    grids[r, c].numberInCol = c;
+                    this.Controls.Add(grids[r,c]);
+                    
+
+                }
+            }
+            updateGridText();
+        }
         public Form1()
         {
             InitializeComponent();
+            CreateSudoku();
         }
 
-    }
+        private void cell_keyPressed(object sender, KeyPressEventArgs e)
+        {
+            var grid = sender as Grid;
+
+            if (Char.IsDigit(e.KeyChar))
+            {
+                 if (e.KeyChar == '0')
+                 {
+                   
+                    numbers[grid.numberInRow, grid.numberInCol] = 0;
+                 }
+                else
+                {
+                    // grid.Text = e.KeyChar.ToString();
+                    numbers[grid.numberInRow, grid.numberInCol] = int.Parse(e.KeyChar.ToString());
+
+                }
+                updateGridText();
+            
+            }
+        }
+     }
 }
